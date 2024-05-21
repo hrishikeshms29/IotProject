d0c8597987653ca63dcd7781ec676f998be0e7dd
sanjay14073 <103362239+sanjay14073@users.noreply.github.com>
2024-05-21 07:43:05 +0530

    Backend Code Refactoring

--- server.js
+++ server.js
@@ -2,31 +2,24 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-// const { SerialPort } = require('serialport');
-// const { Readline } = require('@serialport/parser-readline'); // Correct import statement
 
 const { SerialPort } = require('serialport')
 const { ReadlineParser } = require('@serialport/parser-readline');
 const { Socket } = require('socket.io');
-//const port = new SerialPort('COM12', { baudRate: 9600 }); // replace 'COM12' with your Arduino's serial port
+// replace 'COM12' with your Arduino's serial port
 const port = new SerialPort({
     path: 'COM12',
     baudRate:9600,
     parser: new ReadlineParser("\n")
   });
 
-// var parser = port.pipe(new Readline({ delimiter: '\r\n' }));
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }))
 
 app.use(express.static(__dirname));
 
-// io.on("connection", (socket)=>)
+//Establish socket connection 
 
-// parser.on('data', function(data){
-//   io.emit('arduino:data', data);
-// });
-// Existing code...
 io.on('connection', (socket) => {
     console.log('A user connected');
     parser.on('data', function(data){
@@ -37,8 +30,7 @@ io.on('connection', (socket) => {
     })
 });
   
-
-// parser.on('data', console.log);
+//Set up port configure in the frontend
 
 http.listen(8000, function(){
   console.log('listening on *:8000');
